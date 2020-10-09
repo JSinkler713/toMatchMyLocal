3a8b2d9d94254546e7e461b5226f2916d7df31a1
James Sinkler <jamessinkler713@gmail.com>
2020-10-09 09:12:30 -0700

    adds solution code

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,14 @@ console.log('check check, mic check')
 
 
 // make a new Audio Element
-
-// tie it to an event listener
+//
+const mary = new Audio('./assets/Mary.wav')
 
 // on the event use the play() method
+const playSound = ()=> {
+  mary.play()
+}
+
+// tie it to an event listener
+document.querySelector('button').addEventListener('click', playSound)
+
